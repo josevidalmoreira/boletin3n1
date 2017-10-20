f195fc95a63c6063ae028ceba0aca2dbee4e9e50
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-20 08:56:23 +0200

    Primer comentario

--- src/pruebadecolaboradorjuan/PruebadecolaboradorJuan.java
+++ src/pruebadecolaboradorjuan/PruebadecolaboradorJuan.java
@@ -4,6 +4,7 @@ package pruebadecolaboradorjuan;
 public class PruebadecolaboradorJuan {
     public static void main(String[] args) {
         // Prueba para que arturo clone el repositorio.
+        // Modificación numero 1.
     }
     
 }
