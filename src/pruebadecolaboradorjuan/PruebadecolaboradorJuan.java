cb0758e49fca912ccf588edfebac26850348b92b
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-20 08:57:26 +0200

    Exito de las pruebas realizadas

--- src/pruebadecolaboradorjuan/PruebadecolaboradorJuan.java
+++ src/pruebadecolaboradorjuan/PruebadecolaboradorJuan.java
@@ -5,6 +5,7 @@ public class PruebadecolaboradorJuan {
     public static void main(String[] args) {
         // Prueba para que arturo clone el repositorio.
         // Modificación numero 1.
+        //Exito en las pruebas.
     }
     
 }
